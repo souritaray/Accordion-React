c3d6dff34bfb922bb05dd62c03fb944e0288b441
souritaray <100030958+souritaray@users.noreply.github.com>
2023-06-30 14:54:07 +0530

    setup

---
+++ src/Questions.jsx
@@ -0,0 +1,15 @@
+function Question(){
+    return(
+        <>
+        <div className="question">
+            <header>
+                <h4></h4>
+                <button className="btn"></button>
+            </header>
+            <p></p>
+        </div>
+        </>
+    )
+
+}
+export default Question;
